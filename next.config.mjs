1d5b5d925e7b0b966250d01831f9fda752c181bf
adejsam <adejusamuel>
2024-03-26 16:28:40 +0100

    Hoping for no conflic

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withMDX from "@next/mdx";
+// const withMDX = import('@next/mdx')()
 
-export default nextConfig;
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+// const withMdxFunc = withMDX(nextConfig);
+export default withMDX(nextConfig)();
+// export { withMdxFunc };
+
+// module.exports = withMDX(nextConfig);
+
+// const withMDX = require('@next/mdx')()
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // Configure `pageExtensions` to include MDX files
+//   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+//   // Optionally, add any other Next.js config below
+// }
+
+// module.exports = withMDX(nextConfig)
